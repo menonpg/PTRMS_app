2a1c1e610dd472040410ce0cd4c5e3d89ed298f6
Prahlad G Menon, PhD <menon.prahlad@gmail.com>
2020-12-11 09:23:22 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 MODEL_PATH = "./models/"
 
 MODELS = {
-    "Best of Family Ensemble 01": "StackedEnsemble_BestOfFamily_AutoML_20201201_220644",
-    "Best of Family Ensemble 02": "StackedEnsemble_BestOfFamily_AutoML_20201201_224937",
-    "Complete Ensemble": "StackedEnsemble_AllModels_AutoML_20201201_220644",
+    "Best of Family Ensemble 01": "BOF_01_Dec2_2020",
+    "Best of Family Ensemble 02": "BOF_02_Dec2_2020",
 }
